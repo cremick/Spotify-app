7341ee8853853e1bcf8765f16dc3aa3e9a44dafd
github_id <github_email>
2023-08-03 15:04:49 -0400

    first commit

---
+++ auth.py
@@ -0,0 +1,47 @@
+from dotenv import load_dotenv
+import os
+from requests import post, get
+import base64
+import json
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+
+def get_token():
+    # Urls
+    AUTH_URL = 'https://accounts.spotify.com/authorize'
+    TOKEN_URL = 'https://accounts.spotify.com/api/token'
+
+    # Request User Authorization
+    auth_code = get(AUTH_URL, {
+        'client_id': CLIENT_ID,
+        'response_type': 'code',
+        'redirect_uri': 'http://localhost:8888/callback',
+        'scope': 'playlist-modify-public user-read-private user-read-email',
+    })
+
+    # Get code
+    print(auth_code.url)
+    code = input('Enter code: ')
+
+    # Authorization headers and data
+    auth_header = base64.urlsafe_b64encode((CLIENT_ID + ':' + CLIENT_SECRET).encode())
+
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + str(auth_header, 'utf-8')
+    }
+
+    data = {
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': 'http://localhost:8888/callback',
+    }
+
+    # Request Access Token
+    result = post(url=TOKEN_URL, data=data, headers=headers)
+    json_result = result.json()
+    return json_result['access_token']
